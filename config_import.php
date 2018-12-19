a469ea053ae5c263f539aa802db7ee431f44e479
greyL0rd <martin@martinnilsson.se>
2018-12-20 00:45:23 +0100

    commit med några väntande ändringar efter vila

--- config_import.php
+++ config_import.php
@@ -1,47 +1,103 @@
 <?php
 
+
+
 // import config
-$IMPORT["Rapporter"]["Elever"]["FieldIndex"]["Klass"] = 0; // Klasser
-$IMPORT["Rapporter"]["Elever"]["FieldIndex"]["ElevID"] = 1; // ID (personnummer)
-$IMPORT["Rapporter"]["Elever"]["FieldIndex"]["Efternamn"] = 2; // Elevens efternamn
-$IMPORT["Rapporter"]["Elever"]["FieldIndex"]["Fornamn"] = 3; // Elevens förnamn
+
+$IMPORT["Rapporter"]["Elever"]["FieldIndex"]["Klass"] = 3; // Klasser
+
+$IMPORT["Rapporter"]["Elever"]["FieldIndex"]["ElevID"] = 0; // ID (personnummer)
+
+$IMPORT["Rapporter"]["Elever"]["FieldIndex"]["Efternamn"] = 1; // Elevens efternamn
+
+$IMPORT["Rapporter"]["Elever"]["FieldIndex"]["Fornamn"] = 2; // Elevens förnamn
+
 $IMPORT["Rapporter"]["Elever"]["FieldIndex"]["Larare"] = 4; // Lärare (Mentor)
 
+
+
 $IMPORT["Rapporter"]["Grupper"]["FieldIndex"]["GruppID"] = 0; // ID (Grupper/kurser)
+
 $IMPORT["Rapporter"]["Grupper"]["FieldIndex"]["Larare"] = 1; // Undervisande lärare (kommaseparerade lärar-taggar)
+
 $IMPORT["Rapporter"]["Grupper"]["FieldIndex"]["Elever"] = 2; // Elever (kommaseparerade personnummer)
+
 $IMPORT["Rapporter"]["Grupper"]["FieldIndex"]["Period"] = 3; // termin/hela året
+
 $IMPORT["Rapporter"]["Grupper"]["AllowedPrefixes"] = array(
-"AG15", // språkval
-"15FM/AG/", // 14FM//Em etc ind val
-"15FM//AG/", // fix för skrivfel
-"15EM/AG/",
-"15FMHT/AG/",
-"15FMVT/AG/",
-"AG16", // språkval
-"16FM/AG/", // 14FM//Em etc ind val
-"16EM/AG/",
-"16FMHT/AG/",
-"16FMVT/AG/",
+
+"18FM/AG/", // 14FM//Em etc ind val
+
+"18FM//AG/", // fix för skrivfel
+
+"18EM/AG/",
+
+"18FMHT/AG/",
+
+"18FMVT/AG/",
+
+"AG18", // språkval
+
+"SAG18",
+
+"19FM/AG/", // 14FM//Em etc ind val
+
+"19FM//AG/", // fix för skrivfel
+
+"19EM/AG/",
+
+"19FMHT/AG/",
+
+"19FMVT/AG/",
+
+"AG19", // språkval
+
+"SAG19",
+
 "FNA",
+
 "IB",
+
 "NA",
+
 "TE",
-"TIME"
+
+"TIME",
+
+"IMSPR",
+
+"SVA"
+
 ); 
+
 $IMPORT["Rapporter"]["Grupper"]["DisallowedPostfixes"] = array(
+
 ":a",
+
 ":b"
+
 );
 
+
+
 $IMPORT["Fil"]["ColumnSeparator"] = ";"; // Separerar fält/kolumner i importfilen
+
 $IMPORT["Fil"]["ValueSeparator"] = ","; // Separerar värden i ett fält/kolumn i de fall de innehåller flera värden
+
 $IMPORT["Fil"]["FirstLineIsColumnnames"] = true; 
+
 $IMPORT["Fil"]["PreivewData"] = true; 
+
 $IMPORT["Fil"]["NumPreviewLines"] = 2; 
+
 $IMPORT["Fil"]["MaxFileSize"] = 500000; 
+
 $IMPORT["Fil"]["AllowedExts"] = array("csv", "txt"); 
 
+
+
 $IMPORT["outputLog"] = true; 
+
+
 
 ?>
--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="bs-component">
 			<p>Skoldatans ålder <strong><?php print Kurs::getTimestringSinceLastUpdate() ?></strong> | Datalagrets ålder: <strong><?php print Datalager::getTimestringSinceLastUpdate() ?></strong></p>
-			<p>Produktion: <a href="https://twitter.com/gosuMartin" target="_blank">Martin Nilsson</a> - vid problem eposta <strong>martin.nilsson@karlstad.se</strong> eller ring 0704- 54 64 23 (privat)</p>
+			<p>Produktion: <a href="https://twitter.com/gosuMartin" target="_blank">Martin Nilsson</a> - vid problem eposta <strong>martin.nilsson@karlstad.se</strong> eller ring 054-540 1934 (Martin)</p>
 			<p>Teknik: HTML/CSS/Javascript-framework - <a href="http://getbootstrap.com/" target="_blank">Bootstrap</a> (SASS-versionen) | Javascript-framework - <a href="https://jquery.com/" target="_blank">jQuery</a> | Stilmallsspråk - <a href="http://sass-lang.com/" target="_blank">SASS</a> | SASS-framework - <a href="http://compass-style.org/" target="_blank">Compass</a> | Webbserverspråk - <a href="http://www.php.net/" target="_blank">PHP</a> | Databashanterare - <a href="https://www.mysql.com/" target="_blank">MySQL</a></p>
 			<?php if(Config::DEBUG){ ?>
 				<div class="debug">
